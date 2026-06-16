bd9fa84886a8db6aefe77bf366fb5a4e55a80d98
JosueCR17 <josolorzano4@gmail.com>
2026-06-16 17:13:44 -0600

    cors

--- front-tienda-vinos/src/main.js
+++ front-tienda-vinos/src/main.js
@@ -3,23 +3,12 @@ import { createApp } from 'vue'
 import App    from './App.vue'
 import router from './router'
 import { useCartStore } from './stores/cart'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-
-import 'primeflex/primeflex.css'
-import 'primeicons/primeicons.css'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
 
 // Restaurar el carrito desde localStorage al iniciar la app
 useCartStore().hydrate()
